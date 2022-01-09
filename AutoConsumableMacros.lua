07181ac7bf6fceeadc27c3fdd18f065aaffed886
hjoliveira <7120571+hjoliveira@users.noreply.github.com>
2022-01-09 19:19:52 +0100

    fix bugs

--- AutoConsumableMacros.lua
+++ AutoConsumableMacros.lua
@@ -29,9 +29,6 @@ local consumablesByZone = {
     ["default"] = defaultConsumables
 };
 
--- TODO update on bag item change?
--- TODO support food/water
-
 local function InitMacros()
     if (GetMacroBody("AutoConsHP") == nil) then
         CreateMacro("AutoConsHP", "INV_MISC_QUESTIONMARK", "")
@@ -74,17 +71,18 @@ local function UpdateHealthstoneMacro()
     local mediumCount = 0
     local smallCount = GetItemCount(22103)
 
-    local hsId = largeCount and 22103 or mediumCount and 22103 or smallCount and 22103
+    local hsId = largeCount > 0 and 22103 or mediumCount > 0 and 22103 or smallCount > 0 and 22103
 
-    EditMacro("AutoConsHS", nil, nil, "#showtooltip\n/cast item:" .. hsId)
+    if hsId then
+        EditMacro("AutoConsHS", nil, nil, "#showtooltip\n/cast item:" .. hsId)
+    end
 end
 
 local function UpdateFoodMacros()
-
     local conjuredCount = GetItemCount(34062)
 
-    local foodId = conjuredCount and 34062 or 29448
-    local waterId = conjuredCount and 34062 or 27860
+    local foodId = conjuredCount > 0 and 34062 or 29448
+    local waterId = conjuredCount > 0 and 34062 or 27860
 
     EditMacro("AutoConsFood", nil, nil, "#showtooltip\n/cast item:" .. foodId)
     EditMacro("AutoConsWater", nil, nil, "#showtooltip\n/cast item:" .. waterId)
@@ -98,7 +96,7 @@ AutoConsumableMacros.frame:SetFrameStrata("BACKGROUND")
 
 AutoConsumableMacros.frame:RegisterEvent("PLAYER_LOGIN")
 AutoConsumableMacros.frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-AutoConsumableMacros.frame:RegisterEvent("PLAYER_LEAVE_COMBAT")
+-- AutoConsumableMacros.frame:RegisterEvent("PLAYER_LEAVE_COMBAT")
 AutoConsumableMacros.frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
 
 AutoConsumableMacros.frame:SetScript("OnEvent", function(self, event, ...)
@@ -109,14 +107,11 @@ AutoConsumableMacros.frame:SetScript("OnEvent", function(self, event, ...)
     if (event == "PLAYER_LOGIN" or event == "ZONE_CHANGED_NEW_AREA" or event == "UNIT_INVENTORY_CHANGED") then
         local zone = GetZoneText()
 
-        print(event, zone)
-
         UpdatePotionMacros(zone)
         UpdateFoodMacros()
     end
 
-    -- PLAYER_LEAVE_COMBAT needed?
-    if (event == "PLAYER_LEAVE_COMBAT" or event == "UNIT_INVENTORY_CHANGED") then
+    if (event == "PLAYER_LOGIN" or event == "UNIT_INVENTORY_CHANGED") then
         UpdateHealthstoneMacro()
         UpdateFoodMacros()
     end
